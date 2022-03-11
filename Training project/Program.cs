54307f570731300c7a5205f56b579b84729156d5
niki12120 <niki12120>
2022-03-11 20:48:06 +0200

    First project

---
+++ Training project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Training_project
+{
+    
+}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
